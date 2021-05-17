fae4f17e68f7e5bfe980a83cfa23ee2a094ab4d7
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-05-17 10:35:27 -0400

    revised created our hero dougie

--- donut_adventure.js
+++ donut_adventure.js
@@ -1,43 +1,23 @@
 class Hero {
-  constructor(name){
-    this.health = 100;
-    this.weapons = {
-    sprinkleSpray: 5,
-    sugarShock: 10
-  };
-    this.catchPhrases = ['i\'m fresher than day old pizza',
- 'you can\'t count my calories']
- }
-  talkSass(){
-    console.log(this.catchPhrases[0]);
-  }
-  announceHealth(){
-    console.log(this.health);
-  }
-  fight(){
-    console.log('i\'m ready to rumble');
-  }
-}
-
-class Enemy {
   constructor(name) {
     this.health = 100;
     this.weapons = {
-      pepperoniStars: 5,
-      cheeseGrease: 10
+      sprinkleSpray: 5,
+      sugarShock: 10
     };
-    this.catchPhrases = ['i\'m youtube famous',
-      'i\'m more dangerous than an uncovered sewer']
+    this.catchPhrases = ['i\'m fresher than day old pizza',
+      'you can\'t count my calories']
   }
-
-  talkSmack() {
-    console.log(this.catchPhrases[1]);
+  talkSass() {
+    const heroPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
+    console.log(heroPhrase);
   }
   announceHealth() {
     console.log(this.health);
   }
   fight() {
-    console.log('i\'m gonna flatten you like a slice of pepperoni!');
+    console.log('i\'m ready to rumble');
   }
 }
-const pizzaRat = new Enemy('Pizza Rat');
+
+const dougie = new Hero('Dougie');
