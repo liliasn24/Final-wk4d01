ff6e1c9e38ca84a10a4243fdcf72bb0c4cf703bf
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-05-17 10:55:36 -0400

    revised created our enemy pizza rat

--- donut_adventure.js
+++ donut_adventure.js
@@ -21,3 +21,29 @@ class Hero {
 }
 
 const dougie = new Hero('Dougie');
+
+
+class Enemy {
+  constructor(name) {
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    };
+    this.catchPhrases = ['i\'m youtube famous',
+      'i\'m more dangerous than an uncovered sewer']
+  }
+
+  talkSmack() {
+    const enemyPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
+    console.log(enemyPhrase);
+  }
+  announceHealth() {
+    console.log(this.health);
+  }
+  fight() {
+    console.log('i\'m gonna flatten you like a slice of pepperoni!');
+  }
+}
+
+const pizzaRat = new Enemy('Pizza Rat');
