f65fe58b4d55350d9a27e14210c05a5bc1c9f311
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-05-17 12:37:11 -0400

    dougie and pizza rat have fought

--- donut_adventure.js
+++ donut_adventure.js
@@ -9,18 +9,31 @@ class Hero {
       'you can\'t count my calories']
   }
   talkSass() {
-    const heroPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
+    const heroPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)];
     console.log(heroPhrase);
   }
   announceHealth() {
     console.log(this.health);
   }
-  fight() {
-    console.log('i\'m ready to rumble');
+  fight(enemyP) {
+    // for (let key in this.weapons) {
+    //   console.log(key);
+    //   console.log(this.weapons[key]);
+    let sprinkleSpray = this.weapons.sprinkleSpray;
+    let sugarShock = this.weapons.sugarShock;
+    console.log(sprinkleSpray);
+    console.log(sugarShock);
+    let dvsr = enemyP.health - sugarShock;
+    console.log(dvsr);
+
+    }
   }
-}
+
 
 const dougie = new Hero('Dougie');
+
+
+
 
 
 class Enemy {
@@ -35,15 +48,42 @@ class Enemy {
   }
 
   talkSmack() {
-    const enemyPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
+    const enemyPhrase = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)];
     console.log(enemyPhrase);
   }
   announceHealth() {
     console.log(this.health);
   }
-  fight() {
-    console.log('i\'m gonna flatten you like a slice of pepperoni!');
+  fight(enemyD) {
+    let pepperoniStars = this.weapons.pepperoniStars;
+    let cheeseGrease = this.weapons.cheeseGrease;
+    console.log(pepperoniStars);
+    console.log(cheeseGrease);
+    let rvsd = enemyD.health - pepperoniStars;
+    console.log(rvsd);
+    }
   }
-}
+
 
 const pizzaRat = new Enemy('Pizza Rat');
+
+// Have Dougie talkSass
+console.log(dougie.talkSass());
+
+// Have Pizza Rat talkSmack
+console.log(pizzaRat.talkSmack());
+
+// Have Dougie announceHealth
+console.log(dougie.announceHealth());
+
+// Have Pizza Rat announceHealth
+console.log(pizzaRat.announceHealth());
+
+// Have Pizza Rat fightDougie
+console.log(pizzaRat.fight(dougie));
+
+// Have Dougie fightPizza Rat
+console.log(dougie.fight(pizzaRat));
+
+// Have Pizza Rat and Dougie both announceHealthto make sure their health has decreased!
+console.log(dougie.health); /* how do I access the new 'health' after the fight? */
